d0b7c0e80bd9b71fe233e6ccfa551d41cba55a96
cornalitosalvag <95483183+cornalitosalvag@users.noreply.github.com>
2021-12-28 12:41:40 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,10 +7,10 @@ def create_app():
 
     app.config.from_mapping(
             SECRET_KEY='mikey',
-            DATABASE_HOST=os.environ.get('ble5mmo2o5v9oouq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'),
-            DATABASE_PASSWORD=os.environ.get('tqkb217eerv38ddo'),
-            DATABASE_USER=os.environ.get('u4jofa5m2zaocsu7'),
-            DATABASE=os.environ.get('ir5mplk9rirjxupk'),
+            DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
+            DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
+            DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
+            DATABASE=os.environ.get('FLASK_DATABASE'),
     )
 
     from . import db
